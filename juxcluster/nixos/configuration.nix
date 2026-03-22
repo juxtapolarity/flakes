15ca42a98169d8b819f5a2de22be59b1d0ba997d
jux@nixos <julianfelding@gmail.com>
2026-03-22 21:54:22 +0000

    Remove shared boot from configuration.nix

--- juxcluster/nixos/configuration.nix
+++ juxcluster/nixos/configuration.nix
@@ -7,8 +7,7 @@
   # Bootloader setup
   # ---------------------------------------------------------------------------
   boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.Efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 20;
 
   # ---------------------------------------------------------------------------
