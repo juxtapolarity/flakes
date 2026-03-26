c5bb00ef14dcef161dec077b62ccd4f9e2286ce2
jux@nixos <julianfelding@gmail.com>
2026-03-26 09:46:34 +0000

    Added matter-server compatibility to juxserver

--- juxcluster/nixos/configuration-juxserver.nix
+++ juxcluster/nixos/configuration-juxserver.nix
@@ -15,33 +15,36 @@
   # ---------------------------------------------------------------------------
   services.actual.enable = true;
 
-    # ---------------------------------------------------------------------------
-    # Home Assistant
-    # ---------------------------------------------------------------------------
-    services.home-assistant = {
-      enable = true;
-  
-      config = {
-        homeassistant = {
-          name = "juxserver";
-          unit_system = "metric";
-          time_zone = "Europe/Copenhagen";
-  
-          # helpful when behind Tailscale HTTPS
-          external_url = "https://nixos.tail3aa6f7.ts.net:8443";
-        };
-  
-        default_config = {};
-  
-        http = {
-          use_x_forwarded_for = true;
-          trusted_proxies = [
-            "127.0.0.1"
-            "::1"
-          ];
-        };
+  # ---------------------------------------------------------------------------
+  # Home Assistant
+  # ---------------------------------------------------------------------------
+  services.home-assistant = {
+    enable = true;
+   
+    config = {
+      homeassistant = {
+        name = "juxserver";
+        unit_system = "metric";
+        time_zone = "Europe/Copenhagen";
+   
+        # helpful when behind Tailscale HTTPS
+        external_url = "https://nixos.tail3aa6f7.ts.net:8443";
+      };
+   
+      default_config = {};
+   
+      http = {
+        use_x_forwarded_for = true;
+        trusted_proxies = [
+          "127.0.0.1"
+          "::1"
+        ];
       };
     };
+  };  
+
+  # Enable matter-server
+  services.matter-server.enable = true;
 
   # ---------------------------------------------------------------------------
   # SSH
