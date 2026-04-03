9dae1b7150314acee228a1ca427d196edfbce88e
jux@nixos <julianfelding@gmail.com>
2026-04-03 12:37:46 +0000

    Bugfix printing

--- juxcluster/nixos/configuration-juxserver.nix
+++ juxcluster/nixos/configuration-juxserver.nix
@@ -190,7 +190,7 @@
     browsing = true;
     defaultShared = true;
     listenAddresses = [ "*:631" ];
-    allowFrom = [ "192.168.1.0/24" "localhost" ];
+    allowFrom = [ "all" ];
     webInterface = true;
   };
 
